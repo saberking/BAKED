9e743cfa79bd7269e76c9b6a01f116ab51e9896a
Saber <saberking321@protonmail.com>
2026-09-04 05:08:57 +0100

    editing

--- src/Editor.hpp
+++ src/Editor.hpp
@@ -20,6 +20,7 @@ public:
     SamplePlaybackEnginePolyphonic *engine;
     char name[MAX_FILE_PATH_LENGTH];
     ImPlotSpec spec;
+    bool editMode=false;
 
 
     SampleEditor(const char *_name, AudioData *_data, SamplePlaybackEnginePolyphonic *_engine, Window& window):
@@ -53,27 +54,56 @@ public:
 
         ImGui::SetNextWindowPos(ImVec2(0, 0));
         ImGui::SetNextWindowSize(ImVec2(getWidth(), getHeight()));
+        ImPlotFlags plotFlags = ImPlotFlags_None;
+
 
 
         if (ImGui::Begin("Waveform Analysis", nullptr, ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove)){
             if (data&&ImPlot::BeginPlot("My Plot")) {
+                ImPlotInputMap& input_map = ImPlot::GetInputMap();
+                if (editMode)
+                {
+                    // Remap panning to Right Click cleanly
+                    input_map.Pan = ImGuiMouseButton_Right;
+
+                    // BLOCKS BOX SELECT ZOOM NATIVELY:
+                    // Force the box-select mechanics to require all three modifier keys.
+                    // This frees up normal right-click dragging entirely for panning.
+                    input_map.SelectMod = ImGuiMod_Ctrl ;
+                }
+                else
+                {
+                    // Restore default left-click drag panning and default right-click zooming
+                    input_map.Pan = ImGuiMouseButton_Left;
+
+                    input_map.SelectMod = ImGuiMod_None;
+                }
+
                 ImPlot::SetupAxis(ImAxis_Y1, "Amplitude", ImPlotAxisFlags_Lock);
                 ImPlot::SetupAxisLimits(ImAxis_Y1, -1.0, 1.0, ImPlotCond_Always);
 
                 // Allow the X-axis to scroll and zoom normally
                 ImPlot::SetupAxis(ImAxis_X1, "Samples", ImPlotAxisFlags_None);
                 ImPlot::PlotScatterG("My Line", AtomicVectorGetter, data, data->length, spec);
-                ImPlotPoint mouse_pos = ImPlot::GetPlotMousePos();
                 for(int i=0;i<MAX_POLY;i++){
                     if(engine->engines[i]->playing){
-                        double playhead = engine->engines[i]->playhead; // Pass your actual playback position variable
+                        double playhead = engine->engines[i]->playhead;
                         ImPlot::DragLineX(0, &playhead, ImVec4(0.5,0.5,0.5,0.5), 0.5f, ImPlotDragToolFlags_NoInputs);
+                    }
+                }
+                if (ImPlot::IsPlotHovered() && ImGui::IsMouseDown(ImGuiMouseButton_Left) &&editMode) {
+                    ImPlotPoint current_pos = ImPlot::GetPlotMousePos();
+                    if(current_pos.x>=0&&current_pos.x<MAX_SAMPLE_LENGTH){
+                        data->sampleData[0][current_pos.x]=current_pos.y;
+                        if(data->length<current_pos.x){
+                            data->length=current_pos.x;
+                        }
                     }
                 }
 
 
                 ImPlot::EndPlot();
-
+                ImGui::Checkbox("Edit", &editMode);
             }
         }
 
